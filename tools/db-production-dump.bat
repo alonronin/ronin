3869b2201cd44dc07c894f433687e354d762bcb3
Alon <alon@empeeric.com>
2012-11-07 20:59:30 +0200

    correct gitignore

--- tools/db-production-dump.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-mongodump -u roninil -p ronin1974 -h ds039427.mongolab.com --port 39427 -d roninil
-pause
--- tools/db-restore-local-to-production.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-mongorestore -u roninil -p ronin1974 -h ds039427.mongolab.com --port 39427 -d roninil --drop dump/ronin
-pause
