fb0beb5929022d26606c77b1384ac9c1c760047b
Alon Valadji <alon@ronin.co.il>
2012-11-12 01:55:21 +0200

    recomplile build.min.js

--- grunt.js
+++ grunt.js
@@ -6,14 +6,13 @@ module.exports = function(grunt) {
                     'public/resources/js/jquery.restjson.js',
                     'public/resources/js/jquery.serializeobject.js',
                     'public/resources/js/css_browser_selector.js',
-                    'public/resources/js/script.js',
                     'public/resources/js/jquery.cycle.all.min.js',
                     'public/resources/js/jquery.ui.core.min.js',
                     'public/resources/js/jquery.ui.widget.min.js',
                     'public/resources/js/jquery.ui.tabs.min.js',
                     'public/resources/js/jquery.tools.min.js',
-                    'public/resources/js/jquery.pagination.js',
-                    'public/resources/js/blk.jquery.placeholder.js'
+                    'public/resources/js/blk.jquery.placeholder.js',
+                    'public/resources/js/script.js'
                 ],
                 dest: 'public/resources/js/build.min.js'
             }
